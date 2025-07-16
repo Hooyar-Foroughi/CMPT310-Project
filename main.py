28181782d1178bb29fb70f97746341b59130618e
Victor Nguyen <victornguyen019@gmail.com>
2025-07-16 16:54:15 -0700

    Noted 2 switchable clustering models to test with

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from resemblyzer import preprocess_wav, VoiceEncoder
 from pathlib import Path
 import numpy as np
-from sklearn.cluster import AgglomerativeClustering, KMeans
+from sklearn.cluster import AgglomerativeClustering, KMeans, SpectralClustering
 from sklearn.metrics import silhouette_score
 
 # Load the audio file
 # Replace 'audio/jyirt.wav' with the path to your multi-speaker audio file
-wav_fpath = Path('audio/jyirt.wav') 
+wav_fpath = Path('audio/hiyis.wav') 
 print("File name:", wav_fpath)
 wav = preprocess_wav(wav_fpath)
 # Initialize the VoiceEncoder
@@ -22,6 +22,8 @@ encoder = VoiceEncoder("cpu")
 # A common rate is 16, meaning an embedding every 0.0625 seconds.
 _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
 
+print(wav_splits)
+
 # print(f"Shape of continuous embeddings: {cont_embeds.shape}")
 # print(f"Number of audio splits (segments): {len(wav_splits)}")
 
@@ -30,8 +32,15 @@ highscore=0
 model=None
 print("\nNumber of clusters starting from 2-10")
 for i in range(2,11):
-    cluster_model = AgglomerativeClustering(n_clusters=i)
+    # Poptential clustering models that might work (Test each one)
+    # Agglomerative is faster but possibly less accurate?
+
+    # cluster_model = AgglomerativeClustering(n_clusters=i)
+    cluster_model = SpectralClustering(n_clusters=i)
+
+    # KMeans does not work because spherical radius does not properly capture each distinct voice
     # cluster_model=KMeans(n_clusters=i, n_init='auto')
+
     labels = cluster_model.fit_predict(cont_embeds)
     score=silhouette_score(cont_embeds,labels)
     print(score)

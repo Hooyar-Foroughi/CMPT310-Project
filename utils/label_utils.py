41098605ea5bb64bccdb1e8324b8756c9ab8ba3a
Hooyar-Foroughi <hooyarf@gmail.com>
2025-07-27 19:50:07 -0700

    removing old files

--- utils/label_utils.py
+++
@@ -1,9 +0,0 @@
-
-def count_unique_speakers(rttm_file):
-    speaker_ids = set()
-    with open(rttm_file, 'r') as f:
-        for line in f:
-            parts = line.strip().split()
-            if len(parts) >= 8:
-                speaker_ids.add(parts[7])
-    return len(speaker_ids)
